b993a417297132d6c587c82bec6ff96f4b3eb9a9
Don Myers <donmyers@icloud.com>
2019-02-22 12:49:09 -0500

    updates

--- libraries/Console.php
+++ libraries/Console.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * Orange
+ *
+ * An open source extensions for CodeIgniter 3.x
+ *
+ * This content is released under the MIT License (MIT)
+ * Copyright (c) 2014 - 2019, Project Orange Box
+ */
+
+/**
+ * Console class.
+ *
+ * Wrapper around \League\CLImate\CLImate
+ * provides additional methods
+ *
+ * @package CodeIgniter / Orange
+ * @author Don Myers
+ * @copyright 2019
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @link https://github.com/ProjectOrangeBox
+ * @version v2.0.0
+ * @filesource
+ *
+ * @uses # \League\CLImate\CLImate
+ *
+ */
 
 class Console
 {
